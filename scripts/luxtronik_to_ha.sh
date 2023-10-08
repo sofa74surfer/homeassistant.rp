93cf20b16ba3b20869dcd50015bd60524353e5be
Administrator Rotseepark <administrator@rotseepark.ch>
2023-10-08 10:59:21 +0200

    Update 8.10.23

--- scripts/luxtronik_to_ha.sh
+++ scripts/luxtronik_to_ha.sh
@@ -3,6 +3,9 @@
 luxtronik_ip=$1
 luxtronik_user=$2
 luxtronik_password=$3
+ha_ip=$4
+ha_user=$5
+ha_password=$6
 
 expect <<END
 spawn telnet ${luxtronik_ip}
@@ -11,7 +14,7 @@ send "${luxtronik_user}\r"
 expect "Password:"
 send "${luxtronik_password}\r"
 expect
-send "/mnt/usb/transfer2ha.sh\r"
+send "/mnt/usb/transfer2ha.sh ${ha_ip} ${ha_user} ${ha_password}\r"
 expect
 send "exit\r"
 expect eof
